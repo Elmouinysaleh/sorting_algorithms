b40884e0abd83b5361281b5097d0c3a06eaa00a8
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-11-21 20:07:20 +0100

    Update 0-bubble_sort.c
    Fix gcc errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,37 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - Function that sorts an array of integers in ascending order.
+ *
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int img, moved;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+
+	for (i = 0; i < size - 1; ++i)
+	{
+		moved = 0;
+
+		for (j = 0; j < size - i - 1; ++j)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				img = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = img;
+				moved = 1;
+
+				print_array(array, size);
 			}
 		}
+
+	if (moved == 0)
+	break;
+	}
 }
